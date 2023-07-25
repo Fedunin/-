8d748120d958f7dbe1bb3e0b506884ff1e13316a
Fedyunin <ivan-fed_87@mail.ru>
2023-07-26 01:16:02 +0300

    done task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n > m)
+    Console.WriteLine("Первое число больше второго");
+else if (n < m)
+    Console.WriteLine("Второе число больше первого");
+else
+    Console.WriteLine("Числа равны");
