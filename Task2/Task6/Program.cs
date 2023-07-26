5b6f75d3bc3b599d03b5419ee09b2c7e9bca808c
Fedunin <134494403+Fedunin@users.noreply.github.com>
2023-07-26 03:52:43 +0300

    Done task6

--- Task2/Task6/Program.cs
+++ Task2/Task6/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int remain = num % 2;
+if (remain == 0)
+
+    
+    Console.WriteLine("Чётное");
+
+else
+
+
+    Console.WriteLine("Нечётное");
